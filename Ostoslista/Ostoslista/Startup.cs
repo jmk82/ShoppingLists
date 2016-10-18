50c9d9dad6de9e368327b468bbe3e1dabb1aa01b
Juha-Mikko Koskela <juhamikko@gmail.com>
2016-10-19 01:56:21 +0300

    initial commit

---
+++ Ostoslista/Ostoslista/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ostoslista.Startup))]
+namespace Ostoslista
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
