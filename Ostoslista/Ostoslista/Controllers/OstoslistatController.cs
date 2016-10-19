463ecfceae8603dad8d1cf972aa0c6bf6fe82f48
Juha-Mikko Koskela <juhamikko@gmail.com>
2016-10-19 03:31:53 +0300

    Simple view for shoppinglists

---
+++ Ostoslista/Ostoslista/Controllers/OstoslistatController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Ostoslista.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ostoslista.Controllers
+{
+    public class OstoslistatController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OstoslistatController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Ostoslistat
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var user = _context.Users.Single(u => u.Id == userId);
+            var lists = _context.ShoppingLists.Where(l => l.Owner.Id == user.Id).ToList();
+
+            return View(lists);
+        }
+    }
+}
